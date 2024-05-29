822ffccfdbb53930a62026f1473d2493b692da41
DR. KAMALJIT SINGH SAINI <sainikamaljitsingh@gmail.com>
2024-05-29 16:00:25 +0530

    Update Hello.java
    Added a new Line of Code

--- Hello.java
+++ Hello.java
@@ -4,6 +4,7 @@ public class Hello
     public static ioid main(String args[])
       {
         System.opt.println("Hello World");
+        System.opt.println("Updated the Message") //added new line
       }
   }
     
